6f36c2d28003cc475ce82199da91c940d0fe98a7
Leonardo Salsi <leonardo.salsi@outlook.com>
2023-09-07 10:08:23 +0200

    Previous disk information stored via thread-safe struct. Currently displayed in GUI.

--- src/controller.rs
+++ src/controller.rs
@@ -1,11 +1,8 @@
 use sysinfo::{System, SystemExt};
 use std::collections::HashMap;
-use std::fs::File;
-use std::os::fd;
 use std::thread;
 use std::time::Duration;
-use tempfile::tempfile;
-
+use std::sync::{Arc, Mutex};
 use crate::specs::info::*;
 use crate::specs::capacity::*;
 use crate::specs::processes::*;
@@ -22,7 +19,7 @@ struct Measurements {
     io: Vec<I_O>
 }
 
-fn fetch_measurements(mut temp_file: &File) -> Measurements{
+fn fetch_measurements(diskmonitor: Arc<std::sync::Mutex<DiskMonitor>>) -> Measurements{
     let mut sys = System::new_all();
     sys.refresh_all();
     return Measurements {
@@ -30,27 +27,30 @@ fn fetch_measurements(mut temp_file: &File) -> Measurements{
         capacity: fetch_capacity(&sys),
         processes: fetch_processes(&sys),
         network: fetch_network(&sys),
-        io: fetch_io(&temp_file)
+        io: fetch_io(diskmonitor)
     };
 }
 
 pub fn start_measurements(window_weak: Weak<MainWindow>) {
-    let mut temp_file = tempfile().unwrap();
+    let diskmonitor = Arc::new(Mutex::new(DiskMonitor {
+        prev: HashMap::new(),
+    }));
     thread::spawn(move || {
+        let diskmonitor = Arc::clone(&diskmonitor);
         loop {
             tokio::runtime::Runtime::new().unwrap().block_on(async {
-                measure_system(window_weak.clone(), &temp_file).await;
+                measure_system(window_weak.clone(), diskmonitor.clone()).await;
             });
             thread::sleep(Duration::from_millis(1000));
         }
     });
 }
 
-async fn measure_system(window_weak: Weak<MainWindow>,mut temp_file: &File) {
-    let measurements = fetch_measurements(temp_file);
+async fn measure_system(window_weak: Weak<MainWindow>, diskmonitor: Arc<std::sync::Mutex<DiskMonitor>>) {
+    let measurements = fetch_measurements(diskmonitor);
     window_weak.upgrade_in_event_loop(move |win| {
         win.global::<NetworkAdapter>().set_interfaces(mutate_network(measurements.network));
         win.global::<ProcessesAdapter>().set_processes(mutate_processes(measurements.processes));
-        
+        win.global::<IOAdapter>().set_disks(mutate_io(measurements.io));
     }).unwrap();
 }
--- src/specs/io.rs
+++ src/specs/io.rs
@@ -1,81 +1,76 @@
-
+use std::rc::Rc;
 use std::{collections::HashMap, fs::File};
-use std::io::{self, Write, prelude::*, SeekFrom};
+use std::io::prelude::*;
+use std::sync::Arc;
+use slint::*;
+use crate::ui::*;
 
-use sysinfo::{ProcessExt, System, SystemExt, UserExt, PidExt};
-use serde::{Serialize, Deserialize};
-use serde_json::from_reader;
-
-#[derive(Serialize, Deserialize)]
 pub struct I_O {
     name: String,
     mb_read: f64,
     mb_write: f64
 }
 
-#[derive(Serialize, Deserialize)]
-struct IOMAP(HashMap<String, I_O>);
-
-fn load_map(mut temp_file: &File) -> Result<HashMap<String, I_O>, Box<dyn std::error::Error>> {
-    let mut contents = String::new();
-    temp_file.read_to_string(&mut contents)?;
-
-    let deserialized: IOMAP = serde_json::from_str(&contents)?;
-    let map = deserialized.0;
-
-    Ok(map)
+pub struct DiskMonitor {
+    pub prev: HashMap<String, I_O>,
 }
 
-fn write_map(mut temp_file: &File, map: HashMap<String, I_O>) {
-    let serialized = serde_json::to_string(&IOMAP(map)).unwrap();
-    let mut file = File::create("map.json").unwrap();
-    file.write_all(serialized.as_bytes()).unwrap();
-}
-
-pub fn fetch_io(mut temp_file: &File) -> Vec<I_O> {
-    let mut disks: Vec<I_O> = Vec::new();
-    let mut curr: HashMap<String, I_O>= HashMap::new();
-    match load_map(&temp_file) {
-        Ok(prev) => {
-            // Do something with the map
-        },
-        Err(e) => {
-            eprintln!("An error occurred: {}", e);
-        }
+pub fn mutate_io(disks: Vec<I_O>) -> ModelRc<DiskIO> {
+    let dsks: VecModel<DiskIO> = VecModel::default();
+    for disk in disks {
+        dsks.push(DiskIO { 
+            name: disk.name.into(), 
+            read: disk.mb_read as f32, 
+            write: disk.mb_write as f32
+        });
     }
+    return Rc::new(VecModel::from(dsks)).into();
+}
+
+pub fn fetch_io(diskmonitor: Arc<std::sync::Mutex<DiskMonitor>>) -> Vec<I_O> {
+    let mut disks: Vec<I_O> = Vec::new();
+    let mut curr: HashMap<String, I_O> = HashMap::new();
     let mut io_data = String::new();
     let mut fd = File::open(&"/proc/diskstats").unwrap();
+    let mut diskmonitor = diskmonitor.lock().unwrap();
     fd.read_to_string(&mut io_data).unwrap();
+
     for line in io_data.lines() {
         let fields = line.split_whitespace().collect::<Vec<_>>();
         if fields.len() < 14 {
             continue;
         }
+        if fields[2].contains("loop") || fields[2].contains("ram") {
+            continue;
+        }
         let ds = I_O {
             name: fields[2].to_string(),
             mb_read: fields[5].parse::<f64>().unwrap() / 2048.0,
-            mb_write: fields[9].parse::<f64>().unwrap() / 2048.0,
+            mb_write: fields[9].parse::<f64>().unwrap() / 2048.0
         };
-        if prev.contains_key(fields[2]) {
-            let pds = prev.get(fields[2]).unwrap();
-            let mb_read_s = ds.mb_read - pds.mb_read;
-            let mb_wrtn_s = ds.mb_write - pds.mb_write;
-            let dsk = I_O { name: fields[2].to_owned(), mb_read: mb_read_s, mb_write: mb_wrtn_s };
-            disks.push(dsk);
-
-            curr.insert(fields[2].to_owned(), ds);
-        } else {
-            let dsk = I_O { name: fields[2].to_owned(), mb_read: 0.00, mb_write: 0.00 };
-            disks.push(dsk);
+        unsafe {
+            if diskmonitor.prev.contains_key(fields[2]) {
+                let pds = diskmonitor.prev.get(fields[2]).unwrap();
+                disks.push(I_O { 
+                    name: pds.name.clone(), 
+                    mb_read: ds.mb_read - pds.mb_read, 
+                    mb_write: ds.mb_write - pds.mb_write 
+                });
+            }
         }
-        
+        curr.insert(fields[2].to_owned(), ds);
     }
-    write_map(temp_file, curr);
+    unsafe {
+        diskmonitor.prev = curr;
+    }
+    drop(diskmonitor);
+    disks.sort_by(|a, b| b.mb_read.partial_cmp(&a.mb_read).unwrap());
     print_disks(&disks);
     return disks;
 }
 
 pub fn print_disks(disks: &Vec<I_O>) {
+    println!("Disks:");
     for disk in disks {
         println!("{}: {} MB/s read, {} MB/s write", disk.name, disk.mb_read, disk.mb_write);
     }
